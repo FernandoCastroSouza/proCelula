0f822c1487e92e8841000f95f8ed82567bb7a9dd
Lucas Barque <lucasbarquedasilva@hotmail.com>
2017-06-12 19:59:19 -0400

    Converters

---
+++ app/src/main/java/estacio/br/com/procelula/converter/GrupoEvangelisticoConverter.java
@@ -0,0 +1,45 @@
+package estacio.br.com.procelula.converter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import estacio.br.com.procelula.Dados.GrupoEvangelistico;
+
+public class GrupoEvangelisticoConverter {
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public GrupoEvangelistico fromJson(JSONObject jsonObject) {
+        GrupoEvangelistico obj;
+        try {
+            obj = mapper.readValue(jsonObject.toString(), GrupoEvangelistico.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return obj;
+    }
+
+    public List<GrupoEvangelistico> fromJson(JSONArray jsonArray) {
+        List<GrupoEvangelistico> list = new ArrayList<>();
+        JSONObject objectJson;
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                objectJson = jsonArray.getJSONObject(i);
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+            GrupoEvangelistico obj = fromJson(objectJson);
+            if (obj != null) {
+                list.add(obj);
+            }
+        }
+        return list;
+    }
+}
